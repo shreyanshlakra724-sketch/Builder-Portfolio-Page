e14fdc2f14ec19343857e04e580f51c0f390f45f
V1INTIATIVE <shreyanshlakra724@gmail.com>
2026-09-11 14:29:00 +0530

    COmmit-3

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let navbar_links = document.querySelectorAll('[data-page]');
 let home_page = document.querySelector('#home');
 let about_page = document.querySelector('#about');
 let contact_page = document.querySelector('#contact');
+let cursor = document.querySelector('.cursor');
 
 function revealPage(pageId) {
 
@@ -11,6 +12,8 @@ function revealPage(pageId) {
     });
 
     document.getElementById(pageId).style.display = 'block';
+
+    window.location.hash = pageId;
 }
 
 navbar_links.forEach(link => {
@@ -19,9 +22,57 @@ navbar_links.forEach(link => {
 
         let pageId = link.getAttribute('data-page').toLowerCase();
         revealPage(pageId);
+
+        navbar_links.forEach(link => {
+            link.querySelector('a').classList.remove('active');
+        });
+
+    link.querySelector('a').classList.add('active');
     });
 });
 
+
+
+
+
+document.addEventListener('mousemove', (event) => {
+    cursor.style.left = event.clientX + 'px';
+    cursor.style.top = event.clientY + 'px';
+});
+
+let pageURL = window.location.hash.substring(1);
+
+if (pageURL && document.getElementById(pageURL)) {
+    let pages = [home_page, about_page, contact_page];
+
+    pages.forEach(page => {
+        page.style.display = 'none';
+    });
+
+    document.getElementById(pageURL).style.display = 'block';
+
+    let activeLink = document.querySelector(`[data-page="${pageURL}"]`);
+    activeLink.querySelector('a').classList.add('active');
+}
+else {
+    revealPage('home');
+}
+
+let copy = document.querySelector('#copy');
+
+console.log(copy);
+
+copy.addEventListener('click', () => {
+    navigator.clipboard.writeText('shreyanshlakra724@gmail.com')
+    .then(() => {
+        copy.textContent = 'Copied!';
+    })
+
+    
+    setTimeout(() => {
+        copy.textContent = 'Copy Email';
+    }, 2000);
+})
 
 
 
